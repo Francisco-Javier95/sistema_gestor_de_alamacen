284fc8940f7a415e1404a252c1bc0af591689f8d
Francisco-Javier95 <javsani95@gmail.com>
2026-05-25 20:53:42 -0600

    Crear pagina de registro y logica en app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+# --------------IMPORTACIÓN DE LAS LIBRERIAS------------------
 from flask import Flask 
 from flask import render_template, redirect, request, Response, session
 from flask_mysqldb import MySQL, MySQLdb
 
 app = Flask(__name__,template_folder='template')
 
+# ---------------CONEXIÓN CON BASE DE DATOS-------------------
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
@@ -19,15 +21,15 @@ def home():
 def admin():
     return render_template('admin.html')
 
-# Función de login
-@app.route('/acceso-login', methods=["GET", "POST"])
+# -----------------------Fución login-------------------------
+@app.route('/acceso-login', methods=["GET", "POST"]) # Extrae de la pagina de index.html el formulario con action='acceso-login'
 def login():
-    if request.method == 'POST' and 'usuacorreo' in request.form and 'usuacontra':
-        _usuario = request.form['usuacorreo']
-        _contrasenia = request.form['usuacontra']
+    if request.method == 'POST' and 'usuacorreo' in request.form and 'usuacontra': # Cuando se envien los datos por metodo POST se extraeran los datos de los campos con name="usuacorreo" y name="usuacontra"
+        usuario_usuario = request.form['usuacorreo'] #Se duardan los datos en la variable usuario_usuario
+        usuario_contrasenia = request.form['usuacontra'] #Se guardan los datos en la variable usuario_contrasenia
 
         cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM usuarios WHERE usuario_usuario = %s AND usuario_contrasenia = %s', (_usuario, _contrasenia,))
+        cur.execute('SELECT * FROM usuarios WHERE usuario_usuario = %s AND usuario_contrasenia = %s', (usuario_usuario, usuario_contrasenia,)) #Se selecciona todo de la talba usuarios para comparar los datos de la columna usuario_usuario con el dato que alamacenamos del formulario en la variable usuario_usuario. Y los mismo con los datos de la columna usuario_contrasenia con el dato que se almaceno en la variable usuario_contrasenia
         account = cur.fetchone()
 
         if account:
@@ -35,19 +37,36 @@ def login():
             session['id'] = account['usuario_id']
             session['usuario_privilegio'] = account['usuario_privilegio']
 
-            if session['usuario_privilegio'] == 1:
-                return render_template("admin.html")
-            else:
-                if session['usuario_privilegio'] == 2:
-                    return render_template("usuario.html")
+            if session['usuario_privilegio'] == 1: #Compara el dato del campo de usuario_privilegio del registro para ver si su valor es 1 (1 = Admin)
+                return render_template("admin.html") #Redirige a la interfaz de Admin
+            else: 
+                if session['usuario_privilegio'] == 2: #Sino compara si el dato es igual a 2 (2 = Usuario)
+                    return render_template("usuario.html") #Redirige a la interfaz de Usuario
                 
         else:
-            return render_template('index.html', mensaje="Usuario incorrecto")
+            return render_template('index.html', mensaje_error_credenciales="Usuario incorrecto") 
         
     else:
         return render_template('index.html')
+# ------------------Fin Función login---------------------------
 
+# -------------------Fución registro----------------------------
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
+@app.route('/crear-registro', methods=["GET", "POST"]) # Extrae de la pagina de registro.html el formulario con action='acceso-registro'
+def crear_registro():
+    usuario_usuario=request.form['usuacorreo']
+    usuario_contrasenia=request.form['usuacontra']
+
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO usuarios (usuario_usuario, usuario_contrasenia, usuario_privilegio) VALUES (%s, %s, '2')",(usuario_usuario, usuario_contrasenia))
+    mysql.connection.commit()
+
+    return render_template("index.html", mensaje_registro_exitoso="Usuario registrado exitosamente")
+
+# -----------------Fin Función registro-------------------------
 if __name__ == '__main__':
     app.secret_key="paco_si"
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
